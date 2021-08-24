0af40058e645cebf60c02695498eb0ed86661e2f
Harikrishnan Kannoth <70883415+hxrii@users.noreply.github.com>
2021-08-24 10:21:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('mousemove', parallaxText);
+
+
+function parallaxText(e){
+    this.querySelectorAll('.layer').forEach(layer =>{
+        let x = window.innerWidth - e.pageX*2;
+        layer.style.transform = `translateX(${x}px)`;
+
+    })
+
+}
